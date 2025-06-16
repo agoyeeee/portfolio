9086bf819978d4dc3c477098ddcaacdc2b96b350
agoyeeee <prayogaputra70@gmail.com>
2025-06-16 13:07:33 +0700

    Update featured project in home section to D3MA with new link

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -103,8 +103,8 @@ const home = {
   headline: <>Crafting Digital Experiences with Purpose and Passion</>,
   featured: {
     display: true,
-    title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
+    title: <>Recent project: <strong className="ml-4">D3MA</strong></>,
+    href: "https://manajemenadministrasi.vokasi.uns.ac.id/",
   },
   subline: (
     <>
